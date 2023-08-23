21252713fa77cae84555205a7c73fbc33a835477
unknown <jakinaandriantsoa@gmail.com>
2023-08-23 12:28:06 +0300

    zouera1.1(drag only)

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-var ball = document.getElementById("position");
-var container = document.getElementById("contains");
-//var ballblue = document.getElementById("positionr2");
+// Sélection des jetons
+const jetons = document.querySelectorAll('.jeton');
 
-var x /*left vertical*/= 120;
-var y /*top horizontal*/= 25;
-var dx = 10;
-var dy = 10;
+let jetonSelectionne = null;
+let initialX = 0;
+let initialY = 0;
 
-//alert(" ici "+container.offsetWidth+" et ici "+container.offsetHeight);
-ball.addEventListener('click', () =>{
-    function deplacerBall() {
-        if(x + dx>container.offsetWidth-ball.offsetWidth || x+dx <0){
-            dx = -dx;
-        }
-        if(y + dy>container.offsetHeight-ball.offsetHeight || y + dy <0){
-            dy = -dy;
-        }
+
+
+// Fonction de gestion du début du drag
+function startDrag(event) {
+    jetonSelectionne = event.target;
+    initialX = event.clientX;
+    initialY = event.clientY;
+    jetonSelectionne.classList.add('selected');
+}
+
+// Fonction de gestion du mouvement de la souris
+function drag(event) {
+    if (!jetonSelectionne) return;
     
-        x+=dx;
-        y+=dy;
-        dx-=1;
-        dy-=1;
+    const deltaX = event.clientX - initialX;
+    const deltaY = event.clientY - initialY;
+    // Mettez à jour la position du jeton en fonction du déplacement
+    jetonSelectionne.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+}
 
-        if(dx<=0 && dy<=0)
-            clearInterval(timerld);
+// Fonction de gestion du relâchement
+function stopDrag() {
+    if (!jetonSelectionne) return;
 
-        ball.style.left = x+"px";
-        ball.style.top = y+"px";
-    }   
+    // Ici, vous pouvez appliquer la logique pour déplacer la balle en fonction du vecteur
+    // et réinitialiser la position du jeton
+    
+    jetonSelectionne.classList.remove('selected');
+    jetonSelectionne.style.transform = 'translate(0, 0)';
 
-   var timerld = setInterval(deplacerBall,50);
-})
+    jetonSelectionne = null;
+}
 
+// Ajout des écouteurs d'événements
+jetons.forEach(jeton => {
+    jeton.addEventListener('mousedown', startDrag);
+});
 
+document.addEventListener('mousemove', drag);
+document.addEventListener('mouseup', stopDrag);
